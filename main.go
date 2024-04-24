88228e71411a4cf1aa212e59471ae599d1f079d4
celeste <2895751296@qq.com>
2024-04-24 11:22:11 +0800

    gin restful

--- main.go
+++ main.go
@@ -7,8 +7,32 @@ import (
 
 func main() {
 	router := gin.Default() //携带基础中间件启动
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World")
+	router.GET("/path/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		user := c.DefaultQuery("user", "celeste")
+		pwd := c.Query("pwd")
+		c.JSON(http.StatusOK, gin.H{
+			"id":   id,
+			"user": user,
+			"pwd":  pwd,
+		})
 	})
-	router.Run(":8000")
+	router.POST("/path", func(c *gin.Context) {
+		user := c.DefaultPostForm("user", "celeste")
+		pwd := c.PostForm("pwd")
+		c.JSON(http.StatusOK, gin.H{
+			"user": user,
+			"pwd":  pwd,
+		})
+	})
+	router.DELETE("/path/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.JSON(http.StatusOK, gin.H{
+			"id": id,
+		})
+	})
+	router.PUT("/path", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "Hello World")
+	})
+	router.Run(":8080")
 }
