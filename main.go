ed1dca71d2c84e68d2ade20232778f56ba0dd001
celeste <2895751296@qq.com>
2024-04-24 14:54:58 +0800

    gin bind and file

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
+type PostParams struct {
+	Name string `json:"name" uri:"name" form:"name"`
+	Age  int    `json:"age" uri:"age" form:"age"`
+	Sex  bool   `json:"sex" uri:"sex" form:"sex"`
+}
+
 func main() {
-	router := gin.Default() //携带基础中间件启动
-	router.GET("/path/:id", func(c *gin.Context) {
+	r := gin.Default() //携带基础中间件启动
+	v1 := r.Group("/path")
+	v2 := r.Group("/test")
+	v1.GET("/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		user := c.DefaultQuery("user", "celeste")
 		pwd := c.Query("pwd")
@@ -17,7 +29,7 @@ func main() {
 			"pwd":  pwd,
 		})
 	})
-	router.POST("/path", func(c *gin.Context) {
+	v1.POST("", func(c *gin.Context) {
 		user := c.DefaultPostForm("user", "celeste")
 		pwd := c.PostForm("pwd")
 		c.JSON(http.StatusOK, gin.H{
@@ -25,14 +37,86 @@ func main() {
 			"pwd":  pwd,
 		})
 	})
-	router.DELETE("/path/:id", func(c *gin.Context) {
+	v1.DELETE("/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		c.JSON(http.StatusOK, gin.H{
 			"id": id,
 		})
 	})
-	router.PUT("/path", func(c *gin.Context) {
+	v1.PUT("", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "Hello World")
 	})
-	router.Run(":8080")
+	//=======================================================================
+	//shouldBindJSON
+	v2.POST("/bind", func(c *gin.Context) {
+		var params PostParams
+		err := c.ShouldBindJSON(&params)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status": "failed",
+				"msg":    err,
+				"data":   gin.H{},
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"msg":  "success",
+				"data": params,
+			})
+		}
+	})
+	//shouldBindUri
+	v2.POST("/bind/:name/:age/:sex", func(c *gin.Context) {
+		var params PostParams
+		err := c.ShouldBindUri(&params)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status": "failed",
+				"msg":    err,
+				"data":   gin.H{},
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"msg":  "success",
+				"data": params,
+			})
+		}
+	})
+	//shouldBindWithQuery
+	v2.POST("/bind/query", func(c *gin.Context) {
+		var params PostParams
+		err := c.ShouldBindQuery(&params)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status": "failed",
+				"msg":    err,
+				"data":   gin.H{},
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"msg":  "success",
+				"data": params,
+			})
+		}
+	})
+	//upload
+	v2.POST("/upload", func(c *gin.Context) {
+		//form, _ := c.MultipartForm()
+		//files := form.File["files"] //多文件
+		file, _ := c.FormFile("file")
+		//name := c.PostForm("name")
+		log.Println(file.Filename)
+		in, _ := file.Open()
+		defer in.Close()
+		out, _ := os.Create("./" + file.Filename)
+		io.Copy(out, in)
+		defer out.Close()
+		//c.SaveUploadedFile(file, "./"+file.Filename)
+		//c.JSON(http.StatusOK, gin.H{
+		//	"msg":  file,
+		//	"name": name,
+		//})
+		c.Writer.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename="+file.Filename))
+		c.File("./" + file.Filename)
+	})
+	r.Run(":8080")
 }
