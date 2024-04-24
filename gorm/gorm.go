cfec38ac000c8194f00ae5902c5c04c2179b6a88
celeste <2895751296@qq.com>
2024-04-24 16:15:02 +0800

    Optimize the structure

--- gorm/gorm.go
+++ gorm/gorm.go
@@ -1,5 +1,17 @@
 package gorm
 
-func main() {
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
+func Gorm() {
+	db, err := gorm.Open("mysql", "root:root@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic(err)
+	} else {
+
+	}
+	defer db.Close()
 }
